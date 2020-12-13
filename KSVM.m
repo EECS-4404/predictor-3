56ef17e70359bde34766c1919541828eac415b49
thomas-lykouras <43500676+thomas-lykouras@users.noreply.github.com>
2020-12-12 23:23:00 -0500

    Add preliminary version of KSVM predictor.
    Trains an SVM classifier using a built in matlab function. Runs the classifier on test data and generates a confusion matrix and F1-score.

---
+++ KSVM.m
@@ -0,0 +1,102 @@
+% Kernelized Support Vector Machine
+
+data_training = readtable("dataset_training.txt");
+data_testing = readtable("dataset_testing.txt");
+
+% Extract test labels for F1 Score computation
+test_labels = data_testing(:, 13);
+
+% Train the classifier
+[classifier, score] = trainClassifier(data_training);
+
+% Run classifier on test data
+predictions = classifier.predictFcn(data_testing);
+
+% Generate confusion matrix
+confusionchart(table2array(test_labels), predictions);
+cm = confusionmat(table2array(test_labels), predictions);
+
+TP = cm(1,1);
+FP = cm(2,1);
+FN = cm(1,2);
+
+F1_Score = (2*TP) / (2*TP + FP + FN)
+
+function [trainedClassifier, validationAccuracy] = trainClassifier(trainingData)
+% [trainedClassifier, validationAccuracy] = trainClassifier(trainingData)
+% Returns a trained classifier and its accuracy. This code recreates the
+% classification model trained in Classification Learner app. Use the
+% generated code to automate training the same model with new data, or to
+% learn how to programmatically train models.
+%
+%  Input:
+%      trainingData: A table containing the same predictor and response
+%       columns as those imported into the app.
+%
+%  Output:
+%      trainedClassifier: A struct containing the trained classifier. The
+%       struct contains various fields with information about the trained
+%       classifier.
+%
+%      trainedClassifier.predictFcn: A function to make predictions on new
+%       data.
+%
+%      validationAccuracy: A double containing the accuracy in percent. In
+%       the app, the History list displays this overall accuracy score for
+%       each model.
+%
+% Use the code to train the model with new data. To retrain your
+% classifier, call the function from the command line with your original
+% data or new data as the input argument trainingData.
+%
+% For example, to retrain a classifier trained with the original data set
+% T, enter:
+%   [trainedClassifier, validationAccuracy] = trainClassifier(T)
+%
+% To make predictions with the returned 'trainedClassifier' on new data T2,
+% use
+%   yfit = trainedClassifier.predictFcn(T2)
+%
+% T2 must be a table containing at least the same predictor columns as used
+% during training. For details, enter:
+%   trainedClassifier.HowToPredict
+
+% Extract predictors and response
+% This code processes the data into the right shape for training the
+% model.
+inputTable = trainingData;
+predictorNames = {'Var1', 'Var2', 'Var3', 'Var4', 'Var5', 'Var6', 'Var7', 'Var8', 'Var9', 'Var10', 'Var11', 'Var12'};
+predictors = inputTable(:, predictorNames);
+response = inputTable.Var13;
+
+% Train a classifier
+% This code specifies all the classifier options and trains the classifier.
+classificationSVM = fitcsvm(...
+    predictors, ...
+    response, ...
+    'KernelFunction', 'polynomial', ...
+    'PolynomialOrder', 3, ...
+    'KernelScale', 'auto', ...
+    'BoxConstraint', 1, ...
+    'Standardize', true, ...
+    'ClassNames', [-1; 1]);
+
+% Create the result struct with predict function
+predictorExtractionFcn = @(t) t(:, predictorNames);
+svmPredictFcn = @(x) predict(classificationSVM, x);
+trainedClassifier.predictFcn = @(x) svmPredictFcn(predictorExtractionFcn(x));
+
+% Add additional fields to the result struct
+trainedClassifier.ClassificationSVM = classificationSVM;
+
+% Perform cross-validation
+folds = 5;
+partitionedModel = crossval(trainedClassifier.ClassificationSVM, 'KFold', folds);
+
+% Compute validation predictions
+[validationPredictions, validationScores] = kfoldPredict(partitionedModel);
+
+% Compute validation accuracy
+validationAccuracy = 1 - kfoldLoss(partitionedModel, 'LossFun', 'ClassifError');
+
+end
